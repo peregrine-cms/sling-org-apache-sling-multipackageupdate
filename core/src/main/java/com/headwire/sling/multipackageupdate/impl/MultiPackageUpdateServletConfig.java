887ba34cb61884f7d24f79cf98538a062a975ff5
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-07-17 22:10:14 +0200

    Drop sling_servlet_paths

--- core/src/main/java/com/headwire/sling/multipackageupdate/impl/MultiPackageUpdateServletConfig.java
+++ core/src/main/java/com/headwire/sling/multipackageupdate/impl/MultiPackageUpdateServletConfig.java
@@ -31,9 +31,6 @@ import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 @ObjectClassDefinition(name = "%config.name", description = "%config.description")
 public @interface MultiPackageUpdateServletConfig {
 
-	@AttributeDefinition(name = "%sling.servlet.paths.name", description = "%sling.servlet.paths.description")
-	String[] sling_servlet_paths() default "/bin/multipackageupdate-packages";
-
 	@AttributeDefinition(name = "%server.name", description = "%server.description")
 	String server_url() default "https://vagrant.headwire.com/peregrine";
 
