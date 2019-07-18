c5c560b7f7e9f6c6edc08be153cc9bbbaec0ac09
Radosław Wesołowski <radoslaw.wesolowski@gmail.com>
2019-07-18 16:21:48 +0200

    Add simplest JUNIT cases

---
+++ core/src/test/java/com/headwire/sling/multipackageupdate/PackagesListEndpointTest.java
@@ -0,0 +1,28 @@
+package com.headwire.sling.multipackageupdate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PackagesListEndpointTest {
+
+    private static final String SERVER_URL = "http://localhost:8080/content";
+    private static final String PACKAGES_TXT = "packages.txt";
+    private static final String SAMPLE_PACKAGE = "package-1.0.0.zip";
+
+    private final PackagesListEndpoint model = new PackagesListEndpoint(SERVER_URL, PACKAGES_TXT);
+
+    @Test
+    public void getServerUrl() {
+        Assert.assertEquals(SERVER_URL, model.getServerUrl());
+    }
+
+    @Test
+    public void getFileUrl() {
+        Assert.assertEquals(SERVER_URL + "/" + SAMPLE_PACKAGE, model.getFileUrl(SAMPLE_PACKAGE));
+    }
+
+    @Test
+    public void getPackagesListUrl() {
+        Assert.assertEquals(SERVER_URL + "/" + PACKAGES_TXT, model.getPackagesListUrl());
+    }
+}
